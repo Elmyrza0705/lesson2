14428fce8b130bd41db733592a63acf2263296bd
Elmyrza Maratbekov <elmyrzamaratbekov@MacBook-Pro-Elmyrza.local>
2022-11-17 19:59:21 +0600

    make a variables

---
+++ lesson1.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  String name = 'Erjan';
+  name = "sadsa";
+
+  int age = 21;
+  double weght = 76.5;
+  bool isMan = true;
+
+  dynamic a = true;
+  a = 123;
+  a = 'sdfsd';
+
+  var b = true;
+  b = false;
+
+  final c;
+  c = true;
+
+  const dsa = false;
+
+  List list = [234234, 'thest', true, 'dasdsa'];
+
+  Map p = {
+    "name": "Erjan",
+    "age": 18,
+  };
+
+  var heart_symbol = '\u2665';
+
+  print(heart_symbol);
+}
